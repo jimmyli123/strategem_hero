683c2339574cbe502fc6e9cefb6bf99b6a0154bf
Jimmy Li <lijjimmy@gmail.com>
2024-03-25 00:05:17 -0400

    add basic javascript functionality for the game

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.querySelector('button').addEventListener('click', play)
+let rightArrow = document.querySelector('.fa-solid fa-arrow-right fa-5x')
+console.log('its working')
+
+document.keydown = function (e) {
+    e = e || window.event;
+}
+
+function play() {
+    // Code the 3 second delay
+    // Generate random arrows
+    // If key press does not match arrow, start over
+    console.log('have we started playing?')
+    console.log(Math.ceil(Math.random() * 4))
+    
+}
