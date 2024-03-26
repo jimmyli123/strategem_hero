85fc7b0ee185df362d3f398cfa038d1fb1e4bc19
Jimmy Li <lijjimmy@gmail.com>
2024-03-25 23:16:45 -0400

    find how to read key press

--- js/main.js
+++ js/main.js
@@ -10,7 +10,12 @@ function play() {
     // Code the 3 second delay
     // Generate random arrows
     // If key press does not match arrow, start over
+    
+
     console.log('have we started playing?')
     console.log(Math.ceil(Math.random() * 4))
-    
+    window.addEventListener('keydown', function(e) {
+        console.log(e)
+    })
+    // on next work flow build the random arrow key generator
 }
