b1923cf775358ce7c49148d3e61dfce0a6516e1b
Jimmy Li <lijjimmy@gmail.com>
2024-03-31 22:48:00 -0400

    figure out how to check key input against current arrow

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 document.querySelector('button').addEventListener('click', play)
+
 let rightArrow = document.querySelector('.fa-solid fa-arrow-right fa-5x')
 console.log('its working')
 
-document.keydown = function (e) {
-    e = e || window.event;
+let mapping = {
+    'ArrowRight':39,
+    'ArrowLeft':37,
+    'ArrowUp':38,
+    'ArrowDown':40
 }
 
 function play() {
@@ -14,37 +18,57 @@ function play() {
     let numberOfArrows = Math.ceil(Math.random()*5)
     const showArea = document.querySelector("#show")
     clearShowArea();
+    
+    let answerSheet = []
     for (let i =0; i<numberOfArrows; i++) {
         let key = Math.ceil(Math.random() *4)
+        
         const arrow = document.createElement("i");
+        
         if (key === 1) {
             arrow.classList.add("fa-solid", "fa-arrow-left", "fa-5x")
+            answerSheet.push(37)
         } else if (key === 2) {
             arrow.classList.add("fa-solid", "fa-arrow-right", "fa-5x")
+            answerSheet.push(39)
         } else if (key === 3) {
             arrow.classList.add("fa-solid", "fa-arrow-up", "fa-5x")
+            answerSheet.push(38)
         } else {
             arrow.classList.add("fa-solid", "fa-arrow-down", "fa-5x")
+            answerSheet.push(40)
         }
-
         
         showArea.appendChild(arrow);
     }
-
-
+    console.log(answerSheet)
     console.log('have we started playing?')
-    console.log(Math.ceil(Math.random() * 4))
-    window.addEventListener('keydown', function(e) {
-        console.log(e)
-    })
 
-    // Function will remove all current arrows on the page.
-    function clearShowArea() {
-        const showArea = document.querySelector("#show")
-        let child = showArea.lastElementChild;
-        while (child) {
-            showArea.removeChild(child)
-            child = showArea.lastElementChild;
-        }
+    window.addEventListener('keydown', function(e){
+        if (e.repeat) { return; }
+        console.log(e.keyCode)
+    })
+    console.log(e)
+
+    // for (let i =0; i<answerSheet.length; i++) {
+    //     console.log(`Current keycode is ${e.keyCode}. Answersheet code is ${answerSheet[i]}`)
+    //     if (e.keyCode === answerSheet[i]) {
+    //         console.log('that is correct')
+    //     }
+    //     else {
+    //         console.log('incorrect')
+    //     }
+    // }
+        
+    
+}
+
+// Function will remove all current arrows on the page.
+function clearShowArea() {
+    const showArea = document.querySelector("#show")
+    let child = showArea.lastElementChild;
+    while (child) {
+        showArea.removeChild(child)
+        child = showArea.lastElementChild;
     }
 }
